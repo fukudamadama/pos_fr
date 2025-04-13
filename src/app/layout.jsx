4e0d05dd61a01eff021c3e87d9d33d60af4b9219
fuku-git-423 <ay.fuku.star@gmail.com>
2025-04-14 02:11:16 +0900

    髪質診断と問診ページの実装

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -19,27 +19,32 @@ export default function RootLayout({ children }) {
       >
         {/* ヘッダー（/kamokamo では非表示） */}
         {!isKamokamo && (
-          <header
-            style={{
-              display: "flex",
-              alignItems: "center",
-              marginBottom: "1rem",
-            }}
-          >
-            <Link
-              href="/kamokamo"
-              style={{
-                textDecoration: "none",  
-                color: "inherit",        
-                display: "flex",         
-                alignItems: "center",
-              }}
-            >
-              <h1 style={{ fontSize: "1.25rem", margin: 0, cursor: 'pointer' }}>
-                かもかも診断
-              </h1>
-            </Link>
-          </header>
+          // <header
+          //   style={{
+          //     display: "flex",
+          //     alignItems: "center",
+          //     marginBottom: "1rem",
+          //   }}
+          // >
+          //   <Link
+          //     href="/kamokamo"
+          //     style={{
+          //       textDecoration: "none",  
+          //       color: "inherit",        
+          //       display: "flex",         
+          //       alignItems: "center",
+          //     }}
+          //   >
+          //     <h1 style={{ fontSize: "1.25rem", margin: 0, cursor: 'pointer' }}>
+          //       かもかも診断
+          //     </h1>
+          //   </Link>
+          // </header>
+            <header className="text-center text-lg font-semibold text-[#A2D7DB] border-b-2 border-[#A2D7DB] mb-4 pb-2">
+              <Link href="/kamokamo" className="no-underline text-[#A2D7DB]">
+                kamokamo
+              </Link>
+            </header>
         )}
 
         {children}
